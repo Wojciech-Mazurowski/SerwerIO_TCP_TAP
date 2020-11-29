82f3fd59c79fe67800e5d51c3a63c90c4d2c3b06
wojciooo <50964241+wojciooo@users.noreply.github.com>
2020-11-29 18:18:08 +0100

    dodanie klienta
    dodanie klienta

---
+++ tcpClientWithGUI/tcpClientWithGUI/ConnectionForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace tcpClientWithGUI
+{
+    partial class ConnectionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.ipBox = new System.Windows.Forms.TextBox();
+            this.portBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(318, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Połącz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ipBox
+            // 
+            this.ipBox.Location = new System.Drawing.Point(229, 145);
+            this.ipBox.Name = "ipBox";
+            this.ipBox.Size = new System.Drawing.Size(305, 27);
+            this.ipBox.TabIndex = 1;
+            this.ipBox.Text = "127.0.0.1";
+            // 
+            // portBox
+            // 
+            this.portBox.Location = new System.Drawing.Point(229, 178);
+            this.portBox.Name = "portBox";
+            this.portBox.Size = new System.Drawing.Size(305, 27);
+            this.portBox.TabIndex = 2;
+            this.portBox.Text = "6000";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(202, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(188, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Port";
+            // 
+            // ConnectionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.portBox);
+            this.Controls.Add(this.ipBox);
+            this.Controls.Add(this.button1);
+            this.Name = "ConnectionForm";
+            this.Text = "ConnectionForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox ipBox;
+        private System.Windows.Forms.TextBox portBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ tcpClientWithGUI/tcpClientWithGUI/LoginForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using tcpLogin_Client_LIB;
+
+namespace tcpClientWithGUI
+{
+    public partial class LoginForm : Form
+    {
+
+        private NetworkStream _stream;
+
+        public LoginForm(NetworkStream stream)
+        {
+            _stream = stream;
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+
+            StringBuilder readable = Operations.Login(_stream, userTextBox.Text, passTextBox.Text);
+            if (readable[0] == '1')
+            {
+                MessageBox.Show("Login Succesful.");
+                this.Hide();
+                FuncForm ff = new FuncForm(this, _stream);
+                ff.Show();
+
+            }
+            else { MessageBox.Show("Wrong username or password.");}
+        }
+
+        private void Register_Click(object sender, EventArgs e)
+        {
+            StringBuilder readable = Operations.Register(_stream, userTextBox.Text, passTextBox.Text);
+            if (readable[0] == '1')
+            {
+                MessageBox.Show("Registration Succesful. You can now login.");
+            }
+            else MessageBox.Show("This user already exists!");
+        }
+
+            private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+      
+    }
+}
